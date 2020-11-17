1fff0b6129718f83322d1f2c6356cdc3c8f3d2a6
Diego Prestes <diegoprestes.git@hotmail.com>
2020-11-17 17:54:02 -0300

    feat: moving files automatizated

--- move-files.py
+++ move-files.py
@@ -1,1 +1,28 @@
-#TODO
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+import os
+import json
+import time
+
+folder_to_track = 'thats my folder'
+destination = 'thats where it goes'
+class EventHandler(FileSystemEventHandler):
+  def on_modified(self, event):
+    for filename in os.listdir(folder_to_track):
+      src = folder_to_track + '/' + filename
+      os.rename(src, destination)
+
+folder_to_track = '/Users/diego/Downloads'
+destination = 'Users/diego/Downloads/example_destination'
+event_handler = EventHandler()
+observer = Observer()
+observer.schedule(event_handler, folder_to_track, recursive=True)
+observer.start()
+
+try:
+  while True:
+    time.sleep(10)
+except KeyboardInterrupt:
+  observer.stop()
+observer.join()
