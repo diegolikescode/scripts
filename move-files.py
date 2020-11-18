8964ea64caed31fa8acddf13e6182c69b7715e62
Diego Prestes <diegoprestes.git@hotmail.com>
2020-11-17 21:05:40 -0300

    feat: now it creates the destination file

--- move-files.py
+++ move-files.py
@@ -6,13 +6,15 @@ import time
 import shutil
 class EventHandler(FileSystemEventHandler):
   def on_modified(self, event):
+    if not os.path.exists(destination):
+      os.mkdir(destination)
     for filename in os.listdir(folder_to_track):
       src = folder_to_track + '/' + filename
       final_destination = destination + '/' + filename
       shutil.move(src, final_destination)
 
 folder_to_track = '/Users/diego/Downloads'
-destination = '/Users/diego/Desktop/xqdl'
+destination = '/Users/diego/Documents/destination'
 event_handler = EventHandler()
 observer = Observer()
 observer.schedule(event_handler, folder_to_track, recursive=True)
@@ -20,7 +22,7 @@ observer.start()
 
 try:
   while True:
-    time.sleep(1)
+    time.sleep(3)
 except KeyboardInterrupt:
   observer.stop()
 observer.join()
