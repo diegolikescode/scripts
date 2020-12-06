bc9876d226a562f3f3bbb1752bd541624f869283
Diego Prestes <diegoprestes.git@hotmail.com>
2020-12-06 17:16:19 -0300

    feat: nearly ready to organize the downloads folder

--- auto_organize_downloads.py
+++ auto_organize_downloads.py
@@ -1,1 +1,13 @@
-#TODO
+import os
+import pathlib
+
+for root, dir, file in os.walk ('.'):
+  #filename, file_extension = os.path.splitext(file)
+  for singleFile in file:
+    filename, file_extension = os.path.splitext(singleFile)
+    if file_extension == '.mp4':
+      print(filename, file_extension)
+  
+print('XEGA')
+filename, file_extension = os.path.splitext('./aaaa/aaa.mp4')
+print(filename, file_extension)
