27d6020a1f0128024899745e1ef233c9d65b92f1
Diego Prestes <diegoprestes.git@hotmail.com>
2020-12-06 17:31:12 -0300

    feat: creating the mp4 folder

--- auto_organize_downloads.py
+++ auto_organize_downloads.py
@@ -1,12 +1,20 @@
 import os
-import pathlib
+import shutil
+
+destination = 'D:\downloads\mp4s'
+src = ''
+
+if not os.path.exists(destination):
+  os.mkdir(destination)
 
 for root, dir, file in os.walk ('.'):
   #filename, file_extension = os.path.splitext(file)
   for singleFile in file:
     filename, file_extension = os.path.splitext(singleFile)
     if file_extension == '.mp4':
+      # HERE I GET THE MP4
       print(filename, file_extension)
+
   
 print('XEGA')
 filename, file_extension = os.path.splitext('./aaaa/aaa.mp4')
