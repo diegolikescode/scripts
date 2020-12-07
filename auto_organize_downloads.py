2ddea12c4e3602b226f44e7ee7681ccb99c70faa
Diego Prestes <diegoprestes.git@hotmail.com>
2020-12-06 21:35:58 -0300

    feat: script up n runnin

--- auto_organize_downloads.py
+++ auto_organize_downloads.py
@@ -2,20 +2,28 @@ import os
 import shutil
 
 destination = 'D:\downloads\mp4s'
-src = ''
 
 if not os.path.exists(destination):
   os.mkdir(destination)
 
-for root, dir, file in os.walk ('.'):
-  #filename, file_extension = os.path.splitext(file)
+for root, dirs, file in os.walk ('.'):
   for singleFile in file:
     filename, file_extension = os.path.splitext(singleFile)
     if file_extension == '.mp4':
-      # HERE I GET THE MP4
-      print(filename, file_extension)
+      root = root.split('\\')
+      root = root[1]
+      final_destination = destination + '\\' + singleFile
+      if(os.path.exists(final_destination)):
+        singleFile = '2' + singleFile
+        final_destination = destination + '\\' + singleFile
+      print(os.path.abspath(singleFile))
+      splitedPath = os.path.abspath(singleFile).split('\\')
+      src = os.path.join('\\', splitedPath[0], splitedPath[1], splitedPath[2], splitedPath[3], root, splitedPath[4])
+      src = src.split(':')
+      src = src[0] + ':\\' + src[1]
+      print(dirs)
+      print(src)
+      
+      shutil.move(src, final_destination)
 
-  
-print('XEGA')
-filename, file_extension = os.path.splitext('./aaaa/aaa.mp4')
-print(filename, file_extension)
+print('DONE')
