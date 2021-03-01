775ea87ac8a7dcb8f9804bbccc13d4198e458a66
Diego Prestes (DPS) <diegoprestes.git@hotmail.com>
2021-03-01 14:51:21 -0300

    feat: new script to pull all projects in a folder

--- git_pull.py
+++ git_pull.py
@@ -8,7 +8,5 @@ for root, dirs, files in os.walk(stuff):
     if root[len(stuff):].count(os.sep) < depth:
         allDirs = dirs
 
-print(allDirs)
 for directory in allDirs:
-    os.system(""" cd {0} & call git status & call git add . & call git commit
-" -m 'test: script to commit everything & call git push'""".format(directory))
+    os.system('cd {0} & call git status & call git pull"'.format(directory))
