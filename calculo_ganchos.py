f3712d976309c04c2f1cc83ab12d431705247a07
Diego Prestes (DPS) <diegoprestes.git@hotmail.com>
2021-03-21 19:12:07 -0300

    feat: little command to calculate something for my mother

---
+++ calculo_ganchos.py
@@ -0,0 +1,7 @@
+lado1 = int(input("Digite o valor do primeiro lado (EM CENTIMETROS): "))
+lado2 = int(input("Digite o valor do segundo lado (EM CENTIMETROS): "))
+
+total = ((lado1 * 2) + (lado2 * 2))/30
+print("VALOR TOTAL: ")
+print(total)
+input("Enter pra finalizar")
