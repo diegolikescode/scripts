20a03751235a78d9965f5ab112b01b34e7b4d602
Diego Prestes <diegoprestes.git@hotmail.com>
2021-08-16 19:45:06 -0300

    tryin something

--- anagram.js
+++ anagram.js
@@ -1,6 +1,6 @@
 function main () {
-	const input = ['code', 'doce', 'anagram', 'aanagmr', 'xesqued', 'quesxe']
-	const expectedOutput = ['code', 'anagram', 'xesque', 'xesqued']
+	const input = ['code', 'doce', 'anagram', 'aanagmr', 'xesqued', 'xesque', 'quesxe', 'eodc', 'dcoe']
+	const expectedOutput = ['code', 'anagram', 'xesque']
 	const actualOutput = anagram(input)
 	console.log('input:', input)
 	console.log('expectedOutput:', expectedOutput)
@@ -8,7 +8,19 @@ function main () {
 }
 
 function anagram (words) {
-	// here goes the code
+	const anagrams = []
+	words.forEach(wordForOrder => {
+		const ordenedWord = wordForOrder.split('').sort().join()
+		words.forEach(wordForComparison => {
+			const newWordForComparison = wordForComparison.split('').sort().join()
+			if (ordenedWord === newWordForComparison) {
+				anagrams.push(wordForOrder)
+				return
+			}
+		})
+		flag = false
+	})
+	return anagrams
 }
 
 main()
