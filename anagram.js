9e784b294f3bfad127490ce2cfcfc7b9d195c80d
Diego Prestes <diegoprestes.git@hotmail.com>
2021-08-16 18:16:04 -0300

    add base code to anagram function

---
+++ anagram.js
@@ -0,0 +1,14 @@
+function main () {
+	const input = ['code', 'doce', 'anagram', 'aanagmr', 'xesqued', 'quesxe']
+	const expectedOutput = ['code', 'anagram', 'xesque', 'xesqued']
+	const actualOutput = anagram(input)
+	console.log('input:', input)
+	console.log('expectedOutput:', expectedOutput)
+	console.log('actualOutput:', actualOutput)
+}
+
+function anagram (words) {
+	// here goes the code
+}
+
+main()
