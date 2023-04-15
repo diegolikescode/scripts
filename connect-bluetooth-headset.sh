10e087b95db79b565f013573392a3a801e7ab0c7
diegolikescode <diegoprestes.git@hotmail.com>
2023-04-15 11:50:38 -0300

    TODO

---
+++ connect-bluetooth-headset.sh
@@ -0,0 +1,2 @@
+# TODO: the user will run the script with the name of the headset as a flag (i.e. MY NICE HEADSET) and the script will connect the headset, if it exists, if not, the script will say that didn't found
+# NOTE: bluetoothctl "scan | remove | connect" will all be used for this tiny script 
