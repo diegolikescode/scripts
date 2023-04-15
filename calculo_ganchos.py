89afbe989f5e48b252cb4aa2a9652b83d6966f5f
diegolikescode <diegoprestes.git@hotmail.com>
2023-04-15 12:02:44 -0300

    improve calculo_ganchos

--- calculo_ganchos.py
+++ calculo_ganchos.py
@@ -1,8 +1,12 @@
-while(True):
+runnin = True
+
+while(runnin):
   lado1 = int(input("Digite o valor do primeiro lado (EM CENTIMETROS): "))
   lado2 = int(input("Digite o valor do segundo lado (EM CENTIMETROS): "))
 
-  total = ((lado1 * 2) + (lado2 * 2))/30
-  print("VALOR TOTAL: ")
-  print(total)
-  input("Enter pra finalizar")
+  total = ((lado1 * 2) + (lado2 * 2)) / 30
+  print("VALOR TOTAL: ", str(total))
+  fin = input("aperte N para finalizar")
+
+  if lower(fin) == 'n':
+    runnin = false
